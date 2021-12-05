0efb3268590ef73c5aad3497473e3dffed1b46f1
lusgas-dev <70342651+lusgas-dev@users.noreply.github.com>
2021-12-05 12:53:35 -0300

    Create fly.lua

---
+++ fly.lua
@@ -0,0 +1,11 @@
+repeat wait()
+  until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Torso") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid")
+local mouse = game.Players.LocalPlayer:GetMouse()
+repeat wait() until mouse
+local plr = game.Players.LocalPlayer
+local torso = plr.Character.Torso
+local flying = true
+local deb = true
+local ctrl = {f = 0, b = 0, l = 0, r = 0}
+local lastctrl = {f = 0, b = 0, l = 0, r = 0}
+local maxspeed = 50
